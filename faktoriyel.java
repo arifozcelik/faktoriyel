dfe130bb6f5ea98c96adc6280737fe21ab23414c
arifozcelik <46136397+arifozcelik@users.noreply.github.com>
2021-08-07 13:21:25 +0300

    Add files via upload

---
+++ faktoriyel.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class faktoriyel {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("değer gir");
+        int n = input.nextInt();
+        int total = 1;
+        int kombi;
+
+        for(int i = 1; i <= n; i++){
+            total = total * i;
+            int kombi = total / ()
+        }
+        System.out.println(n + ". Faktöriyel :" +total);
+
+    }
+}
